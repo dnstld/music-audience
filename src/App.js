dbf0870aba7b271a7371742d7b62e400f885e27c
Denis Toledo <contato@denistoledo.com.br>
2020-12-17 19:45:37 -0300

    Created layout component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import Layout from './layouts/Layout';
+
 import GlobalStyles from './globalStyles';
 
 function App() {
   return (
-    <div className="App">
+    <Layout>
       <GlobalStyles />
-      
+
       <h1>Music Audience</h1>
-    </div>
+    </Layout>
   );
 }
 
---
+++ src/layouts/Layout/index.js
@@ -0,0 +1,19 @@
+function Layout({ children }) {
+  return (
+    <div>
+      <header>
+        Header
+      </header>
+
+      <main>
+        {children}
+      </main>
+
+      <footer>
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default Layout;
