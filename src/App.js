51399b0a0dd092410af449c865a956abfa919939
Denis Toledo <contato@denistoledo.com.br>
2020-12-17 19:39:58 -0300

    Added global styles (reset CSS)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import GlobalStyles from './globalStyles';
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
+      
       <h1>Music Audience</h1>
     </div>
   );
